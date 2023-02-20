1053eb3a70fde73438ecc58a5ea3fa2426280888
pratapshingane18 <sushrutrao465@gmail.com>
2023-02-20 21:38:49 +0530

    Intro of tree

--- createBinaryTree.cpp
+++ createBinaryTree.cpp
@@ -1,12 +1,13 @@
 #include<iostream>
+#include<queue>
 using namespace std;
 
 class node
 {
     public:
     int data;
-    int *left;
-    int *right;
+    node* left;
+    node* right;
 
     node(int d){
         this->data = d;
@@ -17,7 +18,11 @@ class node
 };
 
 
-node* builtTree(node * root)
+ 
+
+
+
+node* builtTree(node* root)
 {
     cout<<"Enter Data"<<endl;
 
@@ -25,14 +30,79 @@ node* builtTree(node * root)
     cin>>data;
 
     root = new node(data);
+
+    if(data == -1){
+        return NULL;
+    }
+
+    cout<<"Enter Data for Left Node of"<<data<<endl;
+    root->left = builtTree(root->left);
+
+    cout<<"Enter Data for Right Node of"<<data<<endl;
+    root->right = builtTree(root->right);
+
+    return root;
 }
+
+
+// Level Order Traversal:
+// printing nodes of each Level 
+
+
+void LevelOrderTraversal(node* root){
+    queue<node*> q;
+    q.push(root);
+    q.push(NULL);
+
+    while(!q.empty()){
+        node* temp = q.front();
+      
+        q.pop();
+        
+
+if(temp == NULL){ // Particular Level is Completely Traversed
+    cout<<endl;
+
+    if(!q.empty()){ // queue still has some child nodes
+        q.push(NULL);
+    }
+}
+
+else{
+     cout<<temp->data << " ";
+        if(temp->left){
+            q.push(temp->left);
+
+        }
+
+        if(temp->right)
+        {
+            q.push(temp->right);
+        }
+
+}
+    }
+
+    
+}
+
+
+
+
 
 int main(){
 
-    node * root = NULL;
+    node* root = NULL;
+    // 1 3 7 -1 -1 11 -1 -1 5 17 -1 -1 -1
     //creating a tree
     root = builtTree(root);
 
+cout<<endl;
+cout<<endl;
+
+    cout<<"Printing Level Order Traversal"<<endl;
+    LevelOrderTraversal(root);
 
     return 0;
 }
+
