335ae09fbb43cb19dec84999b94ed6d030ee1b44
pratapshingane18 <sushrutrao465@gmail.com>
2023-02-20 19:24:53 +0530

    Starter

--- Intro.cpp
+++ Intro.cpp
@@ -1,1 +1,43 @@
-Hey
+/*
+Tree is an Non Linear Data Structure
+
+Terms:
+Root,
+Node,
+Children,
+Parent,
+Siblings,
+Ancestor,
+Descendent,
+Leaf
+
+Node:
+Integer data is stored in each Node...
+
+Root:
+Top Node of the Tree.
+Parent of all Nodes in a tree.
+
+Children:
+Descendent of that Node
+
+Sibling:
+Node having same parents.
+
+Ancestor:
+Any Node that Lies while traversing from that Node to Root Node.
+
+Descendent:
+Travelling from Node downwards 
+
+Leaf
+Which dont have any Child.
+
+
+
+
+
+
+
+
+*/
