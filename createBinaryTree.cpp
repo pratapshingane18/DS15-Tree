72adf74538c054964be50085e24832d532c09c15
pratapshingane18 <sushrutrao465@gmail.com>
2023-02-20 23:36:55 +0530

    preorder

--- createBinaryTree.cpp
+++ createBinaryTree.cpp
@@ -99,6 +99,20 @@ void inorder(node* root) {
 
 }
 
+void preorder(node* root) {
+    //base case
+    if(root == NULL) {
+        return ;
+    }
+
+    cout << root-> data << " ";
+    preorder(root->left);
+    preorder(root->right);
+
+}
+
+
+
 
 
 
